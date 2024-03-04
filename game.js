8a41c71c1548891e96bf155791f8073c2b545c5a
vinicius ibiapina <159062806+Viniciusibin@users.noreply.github.com>
2024-03-03 22:13:36 -0300

    projeto individual

--- game.js
+++ game.js
@@ -1,12 +1,15 @@
 var gameState = {
+   // definindo tamanho do jogo igual a largura da tela 
     larguraJogo: window.innerWidth,
     alturaJogo: window.innerHeight,
   };
   
   var config = {
     type: Phaser.AUTO,
+    //definindo no jogo
     width: gameState.larguraJogo,
     height: gameState.alturaJogo,
+    //definindo a física
     physics: {
       default: "arcade",
       arcade: {
